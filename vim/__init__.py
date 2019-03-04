97a6c944ce34b64a4b2e4feedee3e9dd675b5d3c
Zach Philipchook <z.pilipchuk@gmail.com>
2019-03-04 13:39:24 +0300

    gvimrc is not needed

--- vim/__init__.py
+++ vim/__init__.py
@@ -4,7 +4,6 @@ from ..installer import Package as _Package
 vim = _Package(
     __name__,
     {
-        'vimrc': '~/.vim/vimrc',
-        'gvimrc': '~/.gvimrc'
+        'vimrc': '~/.vim/vimrc'
     }
 )
